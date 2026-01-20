425ed084c4baff791633ecbdd3647e6f7e5ca3a1
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-20 23:59:39 +0530

    refactor logoutUser,  setUser, updateEmailVerified methods and add isLoggedIn, lastVerified fields in userStore

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,10 +1,12 @@
+import { apiClient } from "@/lib/apiClient";
+import { toast } from "sonner";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 
 type Role = "attendee" | "admin" | "organizer";
 
-type User = {
+export type User = {
   email: string;
   name: string;
   isEmailVerified: boolean;
@@ -13,18 +15,63 @@ type User = {
 
 type UserStore = {
   user: User | null;
+  isLoggedIn: boolean;
+  lastVerified: Date;
+
   setUser: (user: User) => void;
   logoutUser: () => void;
   updateIsEmailVerified: () => void;
+  verifyAuth: () => Promise<void>;
 }
 
 export const useUserStore = create<UserStore>()(
   persist(
     (set) => ({
       user: null,
-      setUser: (user) => set({ user }),
-      logoutUser: () => set({ user: null }),
-      updateIsEmailVerified: () => set(state => ({ user: { ...state.user, isEmailVerified: true } as User}))
+      isLoggedIn: false,
+      lastVerified: new Date(),
+
+      setUser: (user) => {
+        set({
+          user,
+          lastVerified: new Date(),
+          isLoggedIn: true
+        })
+      },
+
+      logoutUser: () => {
+        try {
+          apiClient.post("/api/users/logout");
+          set({ user: null, isLoggedIn: false })
+          localStorage.removeItem("user-storage")
+          toast.success("Logged out successfully")
+        } catch (error) {
+          toast.error("Failed to logout")
+        }
+      },
+
+      updateIsEmailVerified: () => {
+        set(state => ({ user: { ...state.user, isEmailVerified: true } as User }))
+      },
+
+      verifyAuth: async () => {
+        try {
+          const { data: { data: {user} } } = await apiClient.get("/api/users/current-user");
+          
+          set({
+            user: {
+              email: user.email,
+              name: user.name,
+              isEmailVerified: user.isEmailVerified,
+              role: user.role
+            } as User,
+            isLoggedIn: true
+          });
+        } catch (error: any) {
+          toast.error(error?.response?.data?.message || "Please login to continue");
+          set({ user: null, isLoggedIn: false });
+        }
+      },
     }),
     {
       name: "user-storage",
