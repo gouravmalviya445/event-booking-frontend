26746f6ba9e28a44f13eda956aa9f044459d4945
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-25 23:50:53 +0530

    add update page to redirect to its role dashboard

--- src/app/(root)/(protected)/dashboard/page.tsx
+++ src/app/(root)/(protected)/dashboard/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import { useUserStore } from "@/store/userStore";
+import { useRouter } from "next/navigation";
+
 export default function Dashboard() {
+  const role = useUserStore(state => state.user?.role);
+  const router = useRouter();
+  if (role) {
+    router.push(`/dashboard/${role}`);
+  } else {
+    router.push("/");
+  }
+  
   return (
     <div>Dashboard</div>
   );
